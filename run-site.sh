dc5378564252d776a2bb1b8dcce6fb5aadc74b7b
Jan Sarenik <jajomojo@gmail.com>
2020-02-26 22:48:38 +0100

    run-site.sh: Add better Caddy example

--- run-site.sh
+++ run-site.sh
@@ -3,7 +3,7 @@
 a="/$0"; a=${a%/*}; a=${a:-.}; a=${a#/}/; HERE=$(cd $a; pwd)
 cd $HERE
 PORT=${1:-"8890"}
-MYHOST=${2:-"clock.mysite.eu.org"}
+MYHOST=${2:-"bin.bublina.eu.org"}
 {
 echo "Starting httpd at http://127.0.0.1:$PORT"
 echo
@@ -11,9 +11,15 @@ echo "Example Caddy configuration:"
 } 1>&2
 cat <<EOF
 $MYHOST {
+  root /path/to/public
   proxy / http://127.0.0.1:$PORT {
+    except /css /js /img /robots.txt
     transparent
   }
+  header /css Cache-Control "max-age=2592000"
+  header /js Cache-Control "max-age=2592000"
+  header /img Cache-Control "max-age=2592000"
+  header /robots.txt Cache-Control "max-age=2592000"
   gzip
 }
 EOF
