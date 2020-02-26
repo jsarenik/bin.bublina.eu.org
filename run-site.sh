269051da66ea65ef856e62b4ab99b11082997609
Jan Sarenik <jajomojo@gmail.com>
2020-02-26 23:21:05 +0100

    run-site.sh: extend Caddyfile example with browserconfig.xml

--- run-site.sh
+++ run-site.sh
@@ -13,13 +13,14 @@ cat <<EOF
 $MYHOST {
   root /path/to/public
   proxy / http://127.0.0.1:$PORT {
-    except /css /js /img /robots.txt
+    except /css /js /img /robots.txt /browserconfig.xml
     transparent
   }
   header /css Cache-Control "max-age=2592000"
   header /js Cache-Control "max-age=2592000"
   header /img Cache-Control "max-age=2592000"
   header /robots.txt Cache-Control "max-age=2592000"
+  header /browserconfig.xml Cache-Control "max-age=2592000"
   gzip
 }
 EOF
