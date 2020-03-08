9d0060ccd32edc8315f2861cfbebb66e6fe31f75
Jan Sarenik <jajomojo@gmail.com>
2020-03-08 08:37:44 +0100

    update-scripts.sh: Update also CSS and other link refs

--- update-scripts.sh
+++ update-scripts.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
 
-wget -O - http://127.0.0.1:8890/index.php \
-  | grep '^\s\+<script' \
-  | grep -o 'src="[^"]\+"' \
-  | cut -b5- | tr -d '"' \
-  | while read a
-do
-  wget -O public/${a%\?*} http://127.0.0.1:8890/$a
-done
-
 wget -O gen/index.html http://127.0.0.1:8890/index.php
+
+mkdown() {
+  mkdir -p public/${1%/*}
+  wget -O public/${1%\?*} http://127.0.0.1:8890/$1
+}
+
+catit() {
+  cat gen/index.html
+}
+
+catit \
+  | grep '^\s\+<script' \
+  | grep -o 'src="[^"]\+' \
+  | cut -b6- \
+  | while read a
+do mkdown $a; done
+
+catit \
+  | grep '^\s\+<link' \
+  | grep -o 'href="[^"]\+' \
+  | cut -b7- \
+  | while read a
+do mkdown $a; done
