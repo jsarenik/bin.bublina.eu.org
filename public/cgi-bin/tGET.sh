da78585ec2b95a87caf7be6f07e07e22bbdd94bb
Jan Sarenik <jajomojo@gmail.com>
2020-03-30 22:13:51 +0200

    Get rid of HTTP_COOKIE and lang for now

--- public/cgi-bin/tGET.sh
+++ public/cgi-bin/tGET.sh
@@ -1,8 +1,5 @@
 echo "Content-Type: text/html; charset=UTF-8"
 echo
 echo "<!DOCTYPE html>"
-test "$HTTP_COOKIE" = "" \
-  || eval $(echo "$HTTP_COOKIE" | grep -o '[a-zA-Z]\+=[[:alnum:]]\+')
-test -n "$lang" && lang=" lang=\"$lang\""
-echo "<html$lang>"
+echo "<html>"
 exec cat $HERE/a.html
--- public/cgi-bin/tGETd.sh
+++ public/cgi-bin/tGETd.sh
@@ -1,10 +1,7 @@
 echo "Content-Type: text/html; charset=UTF-8"
 echo
 echo "<!DOCTYPE html>"
-test "$HTTP_COOKIE" = "" \
-  || eval $(echo "$HTTP_COOKIE" | grep -o '[a-zA-Z]\+=[[:alnum:]]\+')
-test -n "$lang" && lang=" lang=\"$lang\""
-echo "<html$lang>"
+echo "<html>"
 TD=$WHERE/$pasteid
 . $TD/env
 . $TD/dt
