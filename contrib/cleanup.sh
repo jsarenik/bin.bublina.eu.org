46ed8d46639a18434577984d68a45efdcf7dbe58
Jan Sarenik <jajomojo@gmail.com>
2021-02-03 22:03:50 +0100

    contrib/cleanup.sh: Working cleanup

--- contrib/cleanup.sh
+++ contrib/cleanup.sh
@@ -2,7 +2,16 @@
 
 # crontab
 # */5 * * * * /path/to/cleanup.sh
+NOW=$(date +%s)
+BINDIR=/tmp/bin
 
-DIR=/tmp/bin
 test -d $DIR || exit 1
-/busybox/find $DIR -mtime +7 -mindepth 1
+/busybox/find $BINDIR -mindepth 1 -maxdepth 1 -type d -name "[0-9a-f]*" |
+  while read i
+  do
+    grep -q EXPIRED $i/env || continue && . $i/env
+    echo $EXPIRED
+    test $EXPIRED -le $NOW && rm -rf $i
+  done
+
+/busybox/find $BINDIR/.limit -type f -mmin +5 -delete
